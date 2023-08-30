44ecf19d58986a5105aae9935bec283efba92287
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-08-30 20:48:42 +0530

    Till For loop

---
+++ for.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+  i:= 2
+  for i <= 2{
+  fmt.Println("Nothing stronger than Family - Dom")
+  i = i + 1
+  }
+  for j:= 7; j < 9; j++{
+  fmt.Println("You don't do that to family - Dom")
+  }
+  for n:= 0; n < 10; n++{
+    if n%2 == 0{
+      fmt.Println(n)
+      fmt.Println("Say no more, Dogs are family - Dom")
+    }
+  }
+
+}
